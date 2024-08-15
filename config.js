350d2c10ce3ee31a773f9299bf2a8448ef53518a
Thomas Stjernegaard Jeppesen <tsjeppesen@gbif.org>
2024-08-15 09:48:42 +0200

    add more hosts to Thrift config

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ console.log('ENV: ' + env);
 
 
 const HBASE = {
-  hosts: ['uc6n10.gbif.org'], //["c4master1-vh.gbif.org", "c4master2-vh.gbif.org", "c3master3-vh.gbif.org"],
+  hosts: ['uc6n10.gbif.org', 'uc6n11.gbif.org', 'uc6n12.gbif.org' ], //["c4master1-vh.gbif.org", "c4master2-vh.gbif.org", "c3master3-vh.gbif.org"],
   port: 31995,
   tableName: 'blast_cache'
   
